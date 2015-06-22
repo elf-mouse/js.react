238f736a4e590fb0399b873bcfaa696ebe6af23c
Elf-mousE <ifmouz@gmail.com>
2015-06-22 22:59:19 +0800

    react

---
+++ webpack-with-less-and-react/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require("gulp"),
+  webpack = require("gulp-webpack-build"),
+  path = require("path"),
+  gutil = require("gulp-util"),
+  moment = require("moment");
+
+gulp.task("script", function() {
+  return _buildTask(false);
+});
+
+gulp.task("watch-script", function() {
+  return _buildTask(true);
+});
+
+function _buildTask(watch) {
+  return gulp.src(webpack.config.CONFIG_FILENAME)
+    .pipe(watch ? webpack.watch(_after) : webpack.run(_after))
+    .pipe(gulp.dest("dist"));
+}
+
+function _after(err, stats) {
+  if (err) {
+    gutil.beep();
+    _logErrors([err]);
+  } else if (stats.compilation.errors.length > 0) {
+    gutil.beep();
+    _logErrors(stats.compilation.errors);
+  } else {
+    gutil.log("Scripts recompiled. Time elapsed: " + moment.duration(stats.endTime - stats.startTime).asSeconds() + "s");
+  }
+}
+
+function _logErrors(errors) {
+  for (var i = 0; i < errors.length; i++) {
+    gutil.log(gutil.colors.red(errors[i]));
+  }
+}
---
+++ webpack-with-less-and-react/webpack.config.js
@@ -0,0 +1,26 @@
+var path = require("path"),
+  webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "dist/bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: "jsx-loader"
+    }, {
+      test: /\.less$/,
+      loader: "style!css!less"
+    }]
+  },
+  plugins: [
+    new webpack.ResolverPlugin([
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+    ])
+  ],
+  resolve: {
+    root: [path.join(__dirname, "bower_components"), path.join(__dirname, "./src")]
+  }
+};
